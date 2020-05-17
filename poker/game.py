3a2f1e247c0d97ebcbd765055ef49ecbf1512efc
Josh Snyder <josh@code406.com>
2020-05-16 20:55:57 -0700

    add mutate call

--- poker/game.py
+++ poker/game.py
@@ -568,6 +568,8 @@ def add_player_balance(name: str, session_id: str, amount: str):
         room.player(session_id).increment_balance(amount)
         return room
 
+    room_state.mutate(mutation)
+
 
 class PlayerGameView(BaseModel):
     next_to_act: str
